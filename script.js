1df0485eecb94d2b4584b04af37942c791f7d803
ANDREI DO ESPÍRITO SANTO FIUZA <24122362@CFP127ALUNO.LOCAL>
2025-04-29 14:06:15 -0300

    producao tela login

---
+++ script.js
@@ -0,0 +1,43 @@
+const loginForm = document.getElementById("loginForm");
+const emailInput = document.getElementById("email");
+const senhaInput = document.getElementById("senha");
+const errorMessage = document.getElementById("errorMessage");
+
+async function loginUser(email, senha) {
+    try {
+        const response = await fetch("https://back-spider.vercel.app/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email, senha })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("userId", data.user.id);
+            alert("Login realizado com sucesso!");
+            window.location.href = ""; // Feed
+        } else {
+            errorMessage.textContent = "Email ou senha inválidos. Tente novamente.";
+        }
+    } catch (error) {
+        console.error("Erro ao tentar fazer login:", error);
+        errorMessage.textContent = "Erro ao conectar ao servidor. Tente novamente mais tarde.";
+    }
+}
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const email = emailInput.value.trim();
+    const senha = senhaInput.value.trim();
+
+    if (!email || !senha) {
+        errorMessage.textContent = "Por favor, preencha todos os campos!";
+        return;
+    }
+
+    loginUser(email, senha);
+});
